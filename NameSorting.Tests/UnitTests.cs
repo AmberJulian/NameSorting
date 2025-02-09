89d8e153d1e4b73af3e088e6a5e01c2d6ed0b420
Amber <amberleejulian@gmail.com>
2025-02-09 16:53:28 +1100

    update: try removing a slash from the name of the file to see if we can find it

--- NameSorting.Tests/UnitTests.cs
+++ NameSorting.Tests/UnitTests.cs
@@ -32,7 +32,7 @@ namespace NameSorting.Tests
         //Note for test marker: Depending on the program intentions, I may not want this unit test. I'd prefer my unit tests don't access real data at all, so there is no chance of destroying it. However, if this is just an internal tool and we can get the data back easily, then this should be helpful to see if someone deleted or moved the file from the location we expect. 
         public void CheckThatFileExists()
         {
-            string filePath = FileReadWriter.GetFinalPath("/unsorted-names-list.txt");
+            string filePath = FileReadWriter.GetFinalPath("unsorted-names-list.txt");
             if (!File.Exists(filePath))
             {
                 Console.WriteLine($"This is the file path we are checking for the unsorted names: {filePath}. Something has gone wrong.");
